949ae01a8caee36e70e92d7d3de3c5c727aa2e8a
Federico Bau <58447627+Koubae@users.noreply.github.com>
2022-01-15 13:59:37 +0100

    Composer json

--- src/App/Lib/DB/Database.php
+++ src/App/Lib/DB/Database.php
@@ -297,9 +297,9 @@ CREATE TABLE IF NOT EXISTS `todo_list` (
 --  ================= < USER TABLES > =================
 CREATE TABLE IF NOT EXISTS `users` (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `email` varchar(249) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-    `username` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `email` varchar(249)  NOT NULL,
+    `password` varchar(255)  NOT NULL,
+    `username` varchar(100)  DEFAULT NULL,
     `status` tinyint(2) unsigned NOT NULL DEFAULT '0',
     `verified` tinyint(1) unsigned NOT NULL DEFAULT '0',
     `resettable` tinyint(1) unsigned NOT NULL DEFAULT '1',
