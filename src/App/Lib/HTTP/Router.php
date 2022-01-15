fcb75c3bb323ac8925323861b60015ce22e9bb22
Federico Bau <58447627+Koubae@users.noreply.github.com>
2022-01-15 11:58:30 +0100

    Composer json

--- src/App/Lib/HTTP/Router.php
+++ src/App/Lib/HTTP/Router.php
@@ -211,7 +211,7 @@ class Router
 
     public static function redirectLogin()
     {
-//        if ($_SERVER['REQUEST_URI'] !== '/login') echo self::redirect(slug: 'login'); // Makes sure that it redirect to the login URI
+        if ($_SERVER['REQUEST_URI'] !== '/login') echo self::redirect(slug: 'login'); // Makes sure that it redirect to the login URI
         return render("login.php", ["logged" => self::logged()]);
     }
 
