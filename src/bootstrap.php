6dc78b0c43676a78f63b8ed0da4c6cb090bfbe1d
Federico Bau <58447627+Koubae@users.noreply.github.com>
2022-01-15 11:15:16 +0100

    Composer json

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,6 +7,8 @@ require realpath(dirname(__DIR__)) . '/src/App/Lib/App.php';
 use App\Lib\{Config, Logger, App};
 $config = Config::getClass(); // pass the class in app and not the instance, learn how.
 $config::initConfig();
+echo "HELLOOOO <br/>";
+exit;
 $logs = $config::getPrivate('logs');
 $logger = Logger::getInstance(key: 'app', log_path:$logs);
 $app = new App(appConfig: $config, logger: $logger);
