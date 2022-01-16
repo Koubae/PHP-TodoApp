fdd80888a5e452c0521bb825618ce4bdced2dc26
Federico Bau <58447627+Koubae@users.noreply.github.com>
2022-01-16 08:17:40 +0100

    Composer json

--- src/App/Lib/DB/todo_list_dump.sql
+++ src/App/Lib/DB/todo_list_dump.sql
@@ -1,22 +1,5 @@
-/*
-*********************************************************************
-Sample SQL Dump file
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`heroku_cd7e75e609cc863` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `heroku_cd7e75e609cc863`;
 
---  Destroy any previously created tables in order to start with a fresh database
 
 CREATE TABLE IF NOT EXISTS `project` (
     id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -143,11 +126,3 @@ CREATE TABLE IF NOT EXISTS `users_throttling` (
     PRIMARY KEY (`bucket`),
     KEY `expires_at` (`expires_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
